9ac5a3a3e178d2c73d797f9206845653b6d0c77d
Marcin-Fraszczak <kolothomasa@wp.pl>
2023-07-08 16:17:59 +0200

    settings.py updated

--- _config/settings.py
+++ _config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', "!!!not_secure!!!")
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -31,6 +31,8 @@ DEBUG = False
 ALLOWED_HOSTS = ['self-testing.up.railway.app', '127:0:0:1']
 
 CSRF_TRUSTED_ORIGINS = ['https://self-testing.up.railway.app/']
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 # Application definition
 
