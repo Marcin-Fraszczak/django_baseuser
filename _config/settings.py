a5cb2cc28ceaaecef31265e9b62b4988276105d1
Marcin-Fraszczak <kolothomasa@wp.pl>
2023-07-11 14:24:09 +0200

    alloed hosts

--- _config/settings.py
+++ _config/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-# ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', '*')]
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', '*')]
+# ALLOWED_HOSTS = ['localhost']
 
 CSRF_TRUSTED_ORIGINS = ['https://self-testing.up.railway.app']
 CSRF_COOKIE_SECURE = True
